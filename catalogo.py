962a203161c2b28fc8895f5a7a030c946c4b1399
morenodiego <morenodiego.alumn@alcobendas.manyanet.org>
2026-01-09 13:17:10 +0100

    creo catalogo

---
+++ catalogo.py
@@ -0,0 +1,17 @@
+catalogo = {
+    "press_banca": {
+        "nombre": "Press de banca",
+        "reps_total": 0,
+        "peso_max": 0
+    },
+    "sentadilla": {
+        "nombre": "Sentadilla",
+        "reps_total": 0,
+        "peso_max": 0
+    },
+    "dominadas": {
+        "nombre": "Dominadas",
+        "reps_max": 0,
+        "peso_max": 0
+    }
+}
