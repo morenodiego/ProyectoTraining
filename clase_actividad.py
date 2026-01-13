2fedbc7266ab85929b31fb098d141960b8d9dff9
morenodiego <morenodiego.alumn@alcobendas.manyanet.org>
2026-01-13 10:42:28 +0100

    clases

--- clase_actividad.py
+++ clase_actividad.py
@@ -3,3 +3,22 @@ class Actividad:
         self.tipo = tipo
         self.duracion = duracion
         self.hora = hora
+
+class Correr (Actividad): 
+    def __init__(self, tipo, duracion, hora,distancia):
+        super().__init__(tipo, duracion, hora)
+        self.distancia = distancia
+
+
+class Cilcismo (Actividad): 
+    def __init__(self, tipo, duracion, hora,distancia,desnivel):
+        super().__init__(tipo, duracion, hora)
+        self.distancia = distancia 
+        self.desnivel = desnivel
+
+class Gimnasio (Actividad): 
+    def __init__(self, tipo, duracion, hora,entreno):
+        super().__init__(tipo, duracion, hora)
+        self.entreno = entreno
+
+    
