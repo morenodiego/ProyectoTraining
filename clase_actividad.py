001c3ad7ba36c52f985bb5765f7fab2774e932fe
hector <vazquezhector.alumn@alcobendas.manyanet.org>
2026-01-13 10:48:57 +0100

    Incluida la clase ciclismo

--- clase_actividad.py
+++ clase_actividad.py
@@ -11,10 +11,14 @@ class Correr (Actividad):
 
 
 class Cilcismo (Actividad): 
-    def __init__(self, tipo, duracion, hora,distancia,desnivel):
+    def __init__(self, tipo, duracion, hora,distancia,altmax,altmin):
         super().__init__(tipo, duracion, hora)
         self.distancia = distancia 
-        self.desnivel = desnivel
+        self.altmax=altmax
+        self.altmin=altmin
+    def calculardesnivel(self, altmax, altmin):
+        self.desnivel=altmax-altmin
+        return self.desnivel
 
 class Gimnasio (Actividad): 
     def __init__(self, tipo, duracion, hora,entreno):
