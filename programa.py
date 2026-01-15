a8582fa36a5778f90b81e64a7356e064e49da03d
Pablo <torrespablo.alumn@alcobendas.manyanet.org>
2026-01-15 11:07:46 +0100

    He creado la metrica

--- programa.py
+++ programa.py
@@ -2,7 +2,7 @@ from registro import registrar,iniciar_sesion
 print("Bienvenido")
 
 menu_inicial = int(input("1.Registrarte\n" \
-"2. iniciar sesion\n"))
+"2. iniciar sesion\n" ))
 
 if menu_inicial == 1: 
     registrar()
