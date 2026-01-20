a922666b8121ec073b0413b2e23564712812f7ea
Pablo <torrespablo.alumn@alcobendas.manyanet.org>
2026-01-20 10:40:10 +0100

    datos completados

--- datos.py
+++ datos.py
@@ -1,40 +1,21 @@
 from metrica import Metrica
-def crear_metrica(): 
+
+def crear_metrica():
     peso = int(input("Dime tu peso: "))
     altura = float(input("Dime tu altura en metros: "))
     edad = int(input("Dime tu edad: "))
+
     metrica = Metrica(peso, altura, edad)
-    print(metrica.calcular_imc())
+    imc = metrica.calcular_imc()
 
-def guardar_metrica(metrica):
-    with open("mediasmetrica.txt", "a") as archivo:
-     
-        
-datos()
+    print("Tu IMC es:", imc)
 
-    # Primero revisamos si el usuario ya existe
-    #with open("medidasmetrica.txt", "r") as archivo:
-    #    for linea in archivo: 
-    #        peso_guardado = linea.split(":")[0] 
-    #        while peso_guardado == peso:
-    #            print("Ya pesabas eso")
-    #            return datos()
-    #        altura_guardado = linea.split(":")[1] 
-    #        while altura_guardado == altura:
-    #            print("Ya tenías esa altura")
-    #            return datos()
-    #        edad_guardado = linea.split(":")[2] 
-    #        while edad_guardado == edad:
-    #            print("Ya tenías esa edad")
-    #            return datos()
-    #        imc_guardado = linea.split(":")[3] 
-    #        while imc_guardado == imc:
-    #            print("Ya tenías ese IMC")
-    #            return datos()
-            
+    guardar_metrica(peso, altura, edad, imc)
 
 
-    # Si no existe, lo guardamos
-    #with open("medidasmetrica.txt", "a") as archivo:
-    #    archivo.write(f"{peso}:{altura}:{edad}:{imc} \n")
-    #print("Datos guardados correctamente")
+def guardar_metrica(peso, altura, edad, imc):
+    with open("medidasmetrica.txt", "a") as archivo:
+        archivo.write(f"{peso}:{altura}:{edad}:{imc}\n")
+    print("Datos guardados correctamente")
+
+crear_metrica()
